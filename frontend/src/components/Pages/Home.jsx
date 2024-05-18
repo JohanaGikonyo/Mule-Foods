16783a9b235baaafa6b22c17bfe524f9983ebd39
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-05-18 16:04:58 +0300

    more

--- frontend/src/components/Pages/Home.jsx
+++ frontend/src/components/Pages/Home.jsx
@@ -10,46 +10,35 @@ function Homes() {
     const [amounts, setAmounts] = useState(Products.map(() => 0));
     const { addItems, increment, decrement } = cartItems();
 
-
     const incrementAmount = (index) => {
-        console.log("Incrementing amount for index:", index);
         setAmounts(prevAmounts => {
             const updatedAmounts = [...prevAmounts];
             updatedAmounts[index] += 1;
-            console.log("Updated amounts:", updatedAmounts);
             return updatedAmounts;
         });
         increment(); // Increment total count
     };
 
     const decrementAmount = (index) => {
-        console.log("Decrementing amount for index:", index);
         if (amounts[index] > 0) {
             setAmounts(prevAmounts => {
                 const updatedAmounts = [...prevAmounts];
                 updatedAmounts[index] -= 1;
-                console.log("Updated amounts:", updatedAmounts);
                 return updatedAmounts;
             });
             decrement(); // Decrement total count
         }
     };
 
-
     const handleAddItemToCart = (item) => {
-        console.log("Item:", item);
         const amount = amounts[item.id - 1];
-        console.log("Amount:", amount);
         if (amount > 0) {
-            // Add item to the cart
-            console.log("Adding item to cart:", item);
             addItems({
                 itemName: item.name,
                 itemPrice: item.price,
                 itemCost: item.price * amount,
                 itemQuantity: amount
             });
-            // Reset the amount to 0 after adding the item to the cart
             setAmounts(prevAmounts => {
                 const updatedAmounts = [...prevAmounts];
                 updatedAmounts[item.id - 1] = 0; // Reset amount after adding to cart
@@ -58,69 +47,54 @@ function Homes() {
         }
     };
 
-
-
-
     return (
-        <div className="grid">
-            {/* <div className={`flex flex-col gap-3 text-center ${productDetailVisible ? 'filter blur-sm' : ''}`}> */}
+        <div className="grid p-4">
             <div>
-                <h1 className="text-2xl italic font-semibold m-5">
+                <h1 className="text-2xl italic font-semibold mb-5 text-center">
                     <span className="text-orange-400">Mule</span> Foods
                 </h1>
-                <p>Click on the + to order Food Item</p>
+                <p className="text-center">Click on the + to order Food Item</p>
             </div>
-            <div className="grid grid-cols-2 lg:grid-cols-3 p-2 m-2 gap-3">
+            <div className="grid grid-cols-2 lg:grid-cols-3 gap-5">
                 {Products.map((item) => (
-                    <div key={item.id} className="inset-0 flex justify-center items-center">
-                        <div className="relative bg-white p-5 rounded-md shadow-lg w-full max-w-md md:max-w-2xl lg:max-w-4xl">
-                            <div className="absolute top-2 right-2"></div>
-                            <div className="flex flex-col lg:flex-row justify-around items-center gap-5">
-                                <div className="flex flex-col justify-around items-center gap-5 m-10">
-                                    <div className="flex flex-row justify-around items-center gap-2">
-                                        <span><small></small></span>
-                                        <p className="">{item.name}</p>
-                                    </div>
-                                    <div className="flex flex-row justify-around items-center gap-2">
-                                        <small><span></span></small>
-                                        <p className="font-extrabold text-2xl rounded-md">
-                                            <span className="text-orange-400 p-2">Ksh.</span>{item.price}
-                                        </p>
-                                    </div>
-                                    <div className="flex gap-5 items-center">
-                                        <button className="flex gap-1 items-center  px-2 py-1">
-                                            <button
-                                                className="text-blue-400 font-extrabold rounded-full  p-1"
-                                                onClick={() => decrementAmount(item.id - 1)}
-                                                disabled={amounts[item.id - 1] === 0}
-                                            >
-                                                <RemoveIcon />
-                                            </button>
-                                            {amounts[item.id - 1]}
-                                            <button
-                                                className="text-blue-400 font-extrabold rounded-full  p-1"
-                                                onClick={() => incrementAmount(item.id - 1)}
-                                            >
-                                                <AddIcon />
-                                            </button>
-                                        </button>
-
-                                    </div>
-                                    <div className="flex gap-3">
-
-                                        <Button disabled={amounts[item.id - 1] === 0} onClick={() => handleAddItemToCart(item)} variant="contained" color="primary">Order</Button>
-                                    </div>
+                    <div key={item.id} className="flex justify-center items-center">
+                        <div className="bg-white p-5 rounded-md shadow-lg w-full max-w-md">
+                            <div className="flex flex-col items-center gap-5">
+                                <p className="text-lg">{item.name}</p>
+                                <p className="font-extrabold text-2xl">
+                                    <span className="text-orange-400">Ksh.</span>{item.price}
+                                </p>
+                                <div className="flex items-center gap-3">
+                                    <button
+                                        className="text-blue-400 font-extrabold rounded-full p-1"
+                                        onClick={() => decrementAmount(item.id - 1)}
+                                        disabled={amounts[item.id - 1] === 0}
+                                    >
+                                        <RemoveIcon />
+                                    </button>
+                                    {amounts[item.id - 1]}
+                                    <button
+                                        className="text-blue-400 font-extrabold rounded-full p-1"
+                                        onClick={() => incrementAmount(item.id - 1)}
+                                    >
+                                        <AddIcon />
+                                    </button>
                                 </div>
+                                <Button
+                                    disabled={amounts[item.id - 1] === 0}
+                                    onClick={() => handleAddItemToCart(item)}
+                                    variant="contained"
+                                    color="primary"
+                                >
+                                    Order
+                                </Button>
                             </div>
                         </div>
                     </div>
                 ))}
-
             </div>
-            <div className="mb-5 p-2">
-                <button>
-                    <NavLink to='/mainpage/help' className="m-5 border p-2 border-orange-400 rounded-md">Help Center</NavLink>
-                </button>
+            <div className="mt-5 text-center">
+                <NavLink to='/mainpage/help' className="border p-2 border-orange-400 rounded-md">Help Center</NavLink>
             </div>
         </div>
     );
