5ee2e3eb98a35076c580addb14036026ef907e83
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-05-27 11:28:31 +0300

    table responsiveness

--- frontend/src/components/Admin/Orders.jsx
+++ frontend/src/components/Admin/Orders.jsx
@@ -96,7 +96,7 @@ function Orders() {
 
     return (
         <div className="p-4">
-            <h1 className='text-3xl font-semibold italic text-orange-900'>Welcome To The Admin Dashboard</h1>
+            <h1 className=' italic text-orange-900'>Welcome To The Admin Dashboard</h1>
             <div className="flex gap-3 m-3 items-center justify-center ">
                 <div className='flex flex-col gap-2'>
                     <div className="bg-gray-200 rounded-lg p-4 w-28 h-20 flex justify-center items-center">
@@ -125,56 +125,65 @@ function Orders() {
                 </div>
             </div>
             <div className="shadow-lg bg-white overflow-x-auto sm:rounded-lg w-full">
-                <table className="min-w-full divide-y divide-gray-200 border border-gray-300 table-auto">
-                    <thead className="bg-gray-50">
-                        <tr>
-                            <th className="px-2 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">No.</th>
-                            <th className="px-2 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">UserName</th>
-                            <th className="px-2 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">Products</th>
-                            <th className="px-2 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">Location</th>
-                            <th className="px-2 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">Time</th>
-                            <th className="px-2 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">Check</th>
-                        </tr>
-                    </thead>
-                    <tbody className="bg-white divide-y divide-gray-200">
-                        {orders.map((order) => {
-                            const orderDate = new Date(order.createdAt);
-
-                            // Format the date
-                            const formattedDate = `${orderDate.getDate()} ${getMonthName(orderDate.getMonth())} ${orderDate.getFullYear()}`;
-
-                            // Format the time
-                            const hours = orderDate.getHours() % 12 || 12; // Convert to 12-hour format
-                            const minutes = orderDate.getMinutes().toString().padStart(2, '0');
-                            const amOrPm = orderDate.getHours() < 12 ? 'AM' : 'PM';
-                            const formattedTime = `${hours}:${minutes} ${amOrPm}`;
-
-                            return (
-                                <tr key={order.id}>
-                                    <td className="px-2 py-2 align-text-top whitespace-normal border border-gray-300">{order.id}</td>
-                                    <td className="px-2 py-2 align-text-top whitespace-normal border border-gray-300">{order.name}</td>
-                                    <td className="px-2 py-2 align-text-top whitespace-normal border border-gray-300">
-                                        {JSON.parse(order.products).map((item) => (
-                                            <div key={item.productName}>{item.productName} - {item.quantity} plates</div>
-                                        ))}
-                                    </td>
-                                    <td className="px-2 py-2 whitespace-normal border border-gray-300">{order.location}</td>
-                                    <td className="px-2 py-2 whitespace-normal border border-gray-300">{formattedDate} {formattedTime}</td>
-                                    <td className="px-2 py-2 whitespace-normal border border-gray-300">
-                                        <input
-                                            type='checkbox'
-                                            checked={completedOrderIds.has(order.id) || order.status === "completed"}
-                                            onChange={() => { handleCheckboxChange(order.id); }}
-                                        />
-                                    </td>
+                <div className="inline-block min-w-full">
+                    <div className="overflow-x-auto">
+                        <table className="min-w-full divide-y divide-gray-200 border border-gray-300 table-auto">
+                            <thead className="bg-gray-50">
+                                <tr>
+                                    <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">No.</th>
+                                    <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">UserName</th>
+                                    <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">Products</th>
+                                    <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider w-40 border border-gray-300">Location</th>
+                                    <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">Time</th>
+                                    <th className="px-4 py-2 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border border-gray-300">Check</th>
                                 </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
-                <h1 className="px-2 py-2 font-semibold border border-gray-300">Total Plates Ordered: {totalQuantity} Plates</h1>
-                <h1 className="px-2 py-2 font-semibold border border-gray-300">Total Cost: Kshs.{totalCost}</h1>
+                            </thead>
+                            <tbody className="bg-white divide-y divide-gray-200">
+                                {orders.map((order) => {
+                                    const orderDate = new Date(order.createdAt);
+
+                                    // Format the date
+                                    const formattedDate = `${orderDate.getDate()} ${getMonthName(orderDate.getMonth())} ${orderDate.getFullYear()}`;
+
+                                    // Format the time
+                                    const hours = orderDate.getHours() % 12 || 12; // Convert to 12-hour format
+                                    const minutes = orderDate.getMinutes().toString().padStart(2, '0');
+                                    const amOrPm = orderDate.getHours() < 12 ? 'AM' : 'PM';
+                                    const formattedTime = `${hours}:${minutes} ${amOrPm}`;
+
+                                    return (
+                                        <tr key={order.id}>
+                                            <td className="px-4 py-2 align-text-top border border-gray-300 whitespace-nowrap">{order.id}</td>
+                                            <td className="px-4 py-2 align-text-top border border-gray-300 whitespace-nowrap">{order.name}</td>
+                                            <td className="px-4 py-2 align-text-top border border-gray-300 max-w-sm whitespace-nowrap break-words">
+                                                {JSON.parse(order.products).map((item) => (
+                                                    <div key={item.productName}>{item.productName} - {item.quantity} plates</div>
+                                                ))}
+                                            </td>
+                                            <td className="px-4 py-2 border border-gray-300  whitespace-nowrap w-72 break-words">{order.location}</td>
+                                            <td className="px-4 py-2 border border-gray-300 whitespace-nowrap">{formattedDate} {formattedTime}</td>
+                                            <td className="px-4 py-2 border border-gray-300 whitespace-nowrap">
+                                                <input
+                                                    type='checkbox'
+                                                    checked={completedOrderIds.has(order.id) || order.status === "completed"}
+                                                    onChange={() => { handleCheckboxChange(order.id); }}
+                                                />
+                                            </td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="mt-4 px-4 py-2 border border-gray-300">
+                    <h1 className="font-semibold">Total Plates Ordered: {totalQuantity} Plates</h1>
+                    <h1 className="font-semibold">Total Cost: Kshs.{totalCost}</h1>
+                </div>
             </div>
+
+
+
         </div >
     );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '72': '18rem', // Add custom width
+        '84': '21rem',
+      }
+    },
   },
   plugins: [],
 }
