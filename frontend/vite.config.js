f49f03d5dcd52162b0ee8807b0e7caebe7a2cafd
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-06-11 14:58:15 +0300

    pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,82 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     'process.env': {},
+//   },
+//   resolve: {
+//     alias: {
+//       '@': '/src',
+//     },
+//   },
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
 export default defineConfig({
-  plugins: [react()],
   define: {
     'process.env': {},
   },
-})
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        "theme_color": "#ffffff",
+        "background_color": "#000000",
+        "orientation": "any",
+        "display": "standalone",
+        "dir": "auto",
+        "lang": "en-US",
+        "name": "mule foods",
+        "short_name": "mule foods app",
+        "icons": [
+          {
+            "src": "favicon.ico",
+            "sizes": "64x64 32x32 24x24 16x16",
+            "type": "image/x-icon"
+          },
+          {
+            "src": "logo192.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          },
+          {
+            "src": "logo512.png",
+            "type": "image/png",
+            "sizes": "512x512"
+          },
+          {
+            "src": "logo256.png",
+            "type": "image/png",
+            "sizes": "256x256"
+          },
+          {
+            "src": "logo384.png",
+            "type": "image/png",
+            "sizes": "384x384"
+          },
+          {
+            "src": "maskable_icon.png",
+            "type": "image/png",
+            "sizes": "192x192",
+            "purpose": "any maskable"
+          }
+        ]
+      }
+    })
+  ]
+});
 
-
-// vite.config.js
 
