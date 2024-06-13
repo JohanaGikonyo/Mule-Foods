2694ce98d293261db9952d01e904025651b575d0
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-06-13 17:41:36 +0300

    icons and footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,19 +1,27 @@
-import Divider from '@mui/material/Divider';
-import CopyrightIcon from '@mui/icons-material/Copyright';
-import { NavLink } from 'react-router-dom';
+import Divider from "@mui/material/Divider";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import { NavLink } from "react-router-dom";
 
 function Footer() {
-    return (
-        <div className='mt-10'>
-            <Divider />
-            <div className='flex justify-between items-center p-5 gap-2'>
-                <small className='text-black flex flex-col items-start justify-around italic'><NavLink to='mainpage/home' className='flex text-black'><span className='text-orange-400 flex'>Mule </span> Foods</NavLink><span className='italic'>Faster than a phone Call</span> </small>
-                <small className='flex items-center justify-around'>
-                    <CopyrightIcon size={5} />copyright{new Date().getFullYear()}
-                </small>
-            </div >
-        </div >
-    );
+  return (
+    <div className="mt-10 mb-5 ">
+      <Divider />
+      <div className="flex justify-between items-center p-5 gap-2 flex-wrap">
+        <small className="text-black flex flex-col items-start justify-around italic">
+          <NavLink to="mainpage/home" className="flex text-black">
+            <span className="text-orange-400 flex">Mule </span> Foods
+          </NavLink>
+          <span className="italic">Faster than a phone Call</span>{" "}
+        </small>
+
+        <small className="flex items-center justify-center text-center">
+          <CopyrightIcon size={5} />
+          copyright{new Date().getFullYear()}
+        </small>
+        <small>Crafted By @Martial School Of IT Students</small>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
