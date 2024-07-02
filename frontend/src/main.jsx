49e9399e489d9c85b3c5755503fb94dbedfccf01
JohanaGikonyo <johanagikonyo552@gmail.com>
2024-07-02 14:47:36 +0300

    build

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-import reportWebVitals from "./reportWebVitals"; // Import the module correctly
+// import reportWebVitals from "./reportWebVitals"; // Import the module correctly
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -22,4 +22,4 @@ serviceWorkerRegistration.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
